d327f524adb341c173ce46091e005cbb8454190d
gopherland <fernand@gopherland.io>
2020-06-11 15:00:08 -0600

    updates

---
+++ profiling/cmd/main.go
@@ -0,0 +1,23 @@
+// Copyright 2020 Imhotep Software
+// All material is licensed under the Apache License Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/gopherland/labs2/profiling/internal"
+)
+
+const httpPort = ":4500"
+
+func main() {
+	http.HandleFunc("/fibr", internal.FibHandler)
+	http.HandleFunc("/fibi", internal.FibHandlerIter)
+
+	log.Printf("[Fib] service is listening on [%s]", httpPort)
+	log.Fatal(http.ListenAndServe(httpPort, nil))
+}
---
+++ profiling/internal/handler.go
@@ -0,0 +1,66 @@
+package internal
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/gopherland/labs2/profiling/internal/fib"
+)
+
+const (
+	contentTypeHDR = "Content-Type"
+	contentType    = "application/json; charset=utf-8"
+)
+
+type Result struct {
+	Number    int `json:"n"`
+	Fibonacci int `json:"fib"`
+}
+
+type Results []Result
+
+func FibHandler(w http.ResponseWriter, r *http.Request) {
+	n, err := strconv.Atoi(r.URL.Query().Get("n"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusExpectationFailed)
+		return
+	}
+
+	var res Results
+	for i := 0; i <= n; i++ {
+		res = append(res, Result{Number: i, Fibonacci: fib.Compute(i)})
+	}
+	buff, err := json.Marshal(&res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set(contentTypeHDR, contentType)
+	if _, err := w.Write(buff); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func FibHandlerIter(w http.ResponseWriter, r *http.Request) {
+	n, err := strconv.ParseInt(r.FormValue("n"), 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusExpectationFailed)
+		return
+	}
+
+	res := make(Results, 0, n)
+	_ = res
+	for i := 0; i <= int(n); i++ {
+		res = append(res, Result{Number: i, Fibonacci: fib.ComputeIter(i)})
+	}
+	buff, err := json.Marshal(&res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set(contentTypeHDR, contentType)
+	if _, err := w.Write(buff); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
